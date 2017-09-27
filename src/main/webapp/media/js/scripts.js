d312c9d376f25b4a62f3a38648d4f87297c7aff7
Sebastian Henschel <sebastian@tesobe.com>
2017-09-27 14:40:56 +0200

    Improved navigation when scrolling #23

--- src/main/webapp/media/js/scripts.js
+++ src/main/webapp/media/js/scripts.js
@@ -1,5 +1,18 @@
-(function(){
+$(function(){
 	'use strict';
 
-
-})();
+	$(window).scroll(function() {
+		var pos = $(this).scrollTop();
+		if (pos > 300) {
+			$('.header').addClass('header-scroll');
+			$('.breadcrumb-section').addClass('breadcrumb-section-scroll');
+			$('.option-section').addClass('option-section-scroll');
+			$('.spacer-section').addClass('spacer-section-scroll');
+		} else {
+			$('.header').removeClass('header-scroll');
+			$('.breadcrumb-section').removeClass('breadcrumb-section-scroll');
+			$('.option-section').removeClass('option-section-scroll');
+			$('.spacer-section').removeClass('spacer-section-scroll');
+		}
+	});
+});
