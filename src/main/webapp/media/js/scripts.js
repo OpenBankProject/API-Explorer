6c6a832db2d5a8f1325e15e54e62effab51e2f01
Sebastian Henschel <sebastian@tesobe.com>
2017-10-02 10:50:36 +0200

    Changed how navbar vs scroll vs anchor is implemented #23

--- src/main/webapp/media/js/scripts.js
+++ src/main/webapp/media/js/scripts.js
@@ -2,17 +2,21 @@ $(function(){
 	'use strict';
 
 	$(window).scroll(function() {
+		console.log('scroll');
 		var pos = $(this).scrollTop();
-		if (pos > 300) {
+		if (pos > 10) {
+			console.log('scrol > 300');
 			$('.header').addClass('header-scroll');
 			$('.breadcrumb-section').addClass('breadcrumb-section-scroll');
+			$('.api-info-section').addClass('api-info-section-scroll');
 			$('.option-section').addClass('option-section-scroll');
-			$('.spacer-section').addClass('spacer-section-scroll');
+			$('.content-box__headline').addClass('content-box__headline-scroll');
 		} else {
 			$('.header').removeClass('header-scroll');
 			$('.breadcrumb-section').removeClass('breadcrumb-section-scroll');
+			$('.api-info-section').removeClass('api-info-section-scroll');
 			$('.option-section').removeClass('option-section-scroll');
-			$('.spacer-section').removeClass('spacer-section-scroll');
+			$('.content-box__headline').removeClass('content-box__headline-scroll');
 		}
 	});
 });
