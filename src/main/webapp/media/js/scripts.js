b4cf9942f128683a4652b51d3584779c41e8f76d
Sebastian Henschel <sebastian@tesobe.com>
2017-10-04 14:53:18 +0200

    Added more changes to navigation and general CSS #23

--- src/main/webapp/media/js/scripts.js
+++ src/main/webapp/media/js/scripts.js
@@ -1,22 +1,3 @@
 $(function(){
 	'use strict';
-
-	$(window).scroll(function() {
-		console.log('scroll');
-		var pos = $(this).scrollTop();
-		if (pos > 10) {
-			console.log('scrol > 300');
-			$('.header').addClass('header-scroll');
-			$('.breadcrumb-section').addClass('breadcrumb-section-scroll');
-			$('.api-info-section').addClass('api-info-section-scroll');
-			$('.option-section').addClass('option-section-scroll');
-			$('.content-box__headline').addClass('content-box__headline-scroll');
-		} else {
-			$('.header').removeClass('header-scroll');
-			$('.breadcrumb-section').removeClass('breadcrumb-section-scroll');
-			$('.api-info-section').removeClass('api-info-section-scroll');
-			$('.option-section').removeClass('option-section-scroll');
-			$('.content-box__headline').removeClass('content-box__headline-scroll');
-		}
-	});
 });
