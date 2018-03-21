1427fbc2dbcf48b48d962c80f60b1f53d0c45783
constantine2nd <marko@tesobe.com>
2018-03-21 11:36:33 +0100

    Enabled version v1 of Berlin Group

--- src/main/scala/code/snippet/ApiExplorer.scala
+++ src/main/scala/code/snippet/ApiExplorer.scala
@@ -357,7 +357,7 @@ WIP to add comments on resource docs. This code copied from Sofit.
     val apiVersionRequested = S.param("version").getOrElse(defaultVersion)
 
 
-    val supportedApiVersions = List("1.2.1", "1.3.0", "1.4.0", "2.0.0", "2.1.0", "2.2.0", "3.0.0")
+    val supportedApiVersions = List("1.2.1", "1.3.0", "1.4.0", "2.0.0", "2.1.0", "2.2.0", "3.0.0", "1")
 
 
     val apiVersion: String = {
