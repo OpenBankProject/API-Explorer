3ac0da2eedd96e256cc2019d9dde70880156b1bf
Marko Milić <marko@tesobe.com>
2020-05-11 08:31:06 +0200

    feature/Use email as the username in case of Google OpenID Connect

--- src/main/scala/code/snippet/Login.scala
+++ src/main/scala/code/snippet/Login.scala
@@ -36,13 +36,23 @@ import net.liftweb.http.js.JsCmd
 import net.liftweb.util.Helpers
 import Helpers._
 import net.liftweb.http.SHtml
-import code.lib.{ObpAPI, OAuthClient}
+import code.lib.{OAuthClient, ObpAPI}
+import net.liftweb.common.Box
 import net.liftweb.http.js.JsCmds.Noop
 
 class Login {
   private def loggedIn = {
+    def getUsername(): Box[String] = {
+      ObpAPI.currentUser.map {
+        u =>
+          u.provider.toLowerCase() match {
+            case provider if provider.contains("google") => u.email
+            case _                                       => u.username
+          }
+      }
+    }
     ".logged-out *" #> "" &
-      ".username *" #> ObpAPI.currentUser.map(u => u.username) &
+    ".username *" #> getUsername() &
     "#logout [onclick+]" #> SHtml.onEvent(s => {
       OAuthClient.logoutAll()
       Noop
