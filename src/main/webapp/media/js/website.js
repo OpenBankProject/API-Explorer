5711af9d1e5aa83e911d61cbef4e205b99de82d2
hongwei <hongwei@tesobe.com>
2020-06-02 14:28:34 +0200

    feature/added the button to show/hide the partial functions

--- src/main/webapp/media/js/website.js
+++ src/main/webapp/media/js/website.js
@@ -5,3 +5,17 @@
 //    "default_topic_url": "http://polarize.it/polarize/socialfinanceapp_55868373368"
 //  });
 //});
+
+// In your Javascript (external .js resource or <script> tag)
+$(document).ready(function() {
+    $("#button-show").click(function(){
+        $(".comma_separated_api_call_list").show();
+        $("#button-hide").show();
+        $("#button-show").hide();
+    });
+    $("#button-hide").click(function(){
+        $(".comma_separated_api_call_list").hide();
+        $("#button-hide").hide();
+        $("#button-show").show();
+    });
+});
