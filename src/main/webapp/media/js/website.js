6cdc91634962621d75165681d2359a45bed8f479
hongwei <hongwei@tesobe.com>
2020-06-16 00:21:16 +0200

    P15-1.12 Expanding container -- use the link there.

--- src/main/webapp/media/js/website.js
+++ src/main/webapp/media/js/website.js
@@ -8,14 +8,4 @@
 
 // In your Javascript (external .js resource or <script> tag)
 $(document).ready(function() {
-    $("#button-show").click(function(){
-        $(".comma_separated_api_call_list").show();
-        $("#button-hide").show();
-        $("#button-show").hide();
-    });
-    $("#button-hide").click(function(){
-        $(".comma_separated_api_call_list").hide();
-        $("#button-hide").hide();
-        $("#button-show").show();
-    });
 });
