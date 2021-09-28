7725bf8ea69c2cb470e280bf0799791d3d8163e0
Hongwei <hongwei@tesobe.com>
2021-09-28 11:20:48 +0200

    feature/Improve Navigation between API Explorer main page and glossary items.-step2

--- src/main/scala/code/snippet/ApiExplorer.scala
+++ src/main/scala/code/snippet/ApiExplorer.scala
@@ -1413,10 +1413,10 @@ WIP to add comments on resource docs. This code copied from Sofit.
             "@api_glossary_item_link [href]" #> s"/glossary?#${i._1}" &
             "@api_glossary_item_link [id]" #> s"glossary-${i._1}" &
             "@api_glossary_item_link * " #>{
-              val description = glossaryItems.find(_.title == i._1).map(_.description.markdown).getOrElse("")
+              val description = glossaryItems.find(_.title == i._1.replaceAll("-"," ")).map(_.description.markdown).getOrElse("")
               if (description.length > 15)
                 description.substring(0,14)+" ..."
-              else if(description.length > 0 && description.length<15)
+              else if(description.length > 0 && description.length<=15)
                 description +" ..."
               else
                 "" //If there is no description, we will show empty here.
@@ -1736,18 +1736,20 @@ WIP to add comments on resource docs. This code copied from Sofit.
       }
     }else{
       ".glossary" #> glossaryItems.map  { i =>
+        //from the tag, we need the oprationId, 
+        val tag = i.title.replaceAll(" ", "-")
+        val operationId = allResourcesList.find(_.tags.head == tag).map(_.operation_id).getOrElse("")
       // append the anchor to the current url. Maybe need to set the catalogue to all etc else another user might not find if the link is sent to them.
       ".end-point-anchor [href]" #> s"#${urlEncode(i.title.replaceAll(" ", "-"))}" &
         ".content-box__headline *" #> i.title &
         ".content-box__headline [id]" #> i.title.replaceAll(" ", "-") & // id for the anchor to find
         //i.title must be a proper tag, and will prepare the URL for it ...
         ".glossary_item_apis [href]" #> {
-          //from the tag, we need the oprationId, 
-          val tag = i.title
-          val operationId = allResourcesList.find(_.tags.head == tag).map(_.operation_id).getOrElse("")
-          s"./?operation_id=${operationId.replace(".","_").replaceAll(" ","_")}"
+          s"./?operation_id=${operationId.replace(".","_").replaceAll(" ","_")}#group-${tag}"
         }&
-        ".glossary_item_apis *" #> {i.title +" APIs"} &
+        ".glossary_item_apis *" #> {
+          if(operationId == "") "" else i.title +" APIs"
+        } &
         // Replace attribute named overview_text with the value (whole div/span element is replaced leaving just the text)
         // This is the main description text. We use the html version of the description
         ".content-box__text-box *" #> stringToNodeSeq(i.description.html)
