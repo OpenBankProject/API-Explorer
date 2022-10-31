c2d025385a68fe1413b1f3feb8f8e6e235783565
Marko Milić <marko@tesobe.com>
2022-10-31 10:24:46 +0100

    feature/Copy Endpoint's Json Response to Clipboard 5

--- src/main/webapp/media/js/website.js
+++ src/main/webapp/media/js/website.js
@@ -278,5 +278,4 @@ function copyJsonResultToClipboard(element) {
   window.getSelection().addRange(r);
   document.execCommand('copy');
   window.getSelection().removeAllRanges();
-  document.getElementById(element.id).title = "Copied";
 }
