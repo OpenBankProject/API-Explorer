1c735bf92630085dc8d2bff8404c52f7d23a7029
Marko Milić <marko@tesobe.com>
2022-11-10 09:20:45 +0100

    feature/Bold the text of a selected language at API Explorer

--- src/main/scala/code/util/I18NUtil.scala
+++ src/main/scala/code/util/I18NUtil.scala
@@ -2,7 +2,9 @@ package code.util
 
 import java.util.{Date, Locale}
 
+import net.liftweb.common.Full
 import net.liftweb.http.S
+import net.liftweb.http.provider.HTTPCookie
 import net.liftweb.util.Props
 
 object I18NUtil {
@@ -21,9 +23,19 @@ object I18NUtil {
   def currentLocale() : Locale = {
     // Cookie name
     val localeCookieName = "SELECTED_LOCALE"
-    S.findCookie(localeCookieName).flatMap {
-      cookie => cookie.value.map(computeLocale)
-    } openOr getLocale()
+    S.param("locale") match {
+      // 1st choice: Use query parameter as a source of truth if any
+      case Full(requestedLocale) if requestedLocale != null => {
+        val computedLocale = I18NUtil.computeLocale(requestedLocale)
+        S.addCookie(HTTPCookie(localeCookieName, requestedLocale))
+        computedLocale
+      }
+      // 2nd choice: Otherwise use the cookie  
+      case _ =>
+        S.findCookie(localeCookieName).flatMap {
+          cookie => cookie.value.map(computeLocale)
+        } openOr getLocale()
+    }
   }
   // Properly convert a language tag to a Locale
   def computeLocale(tag : String) = tag.split(Array('-', '_')) match {
