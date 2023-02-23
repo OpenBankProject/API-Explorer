25412cf692f7626860beeb5fae3c4151c1138a53
Marko Milić <marko@tesobe.com>
2023-02-23 11:45:42 +0100

    feature/Renamed props language_tag to default_locale, default is en_GB

--- src/main/scala/code/util/I18NUtil.scala
+++ src/main/scala/code/util/I18NUtil.scala
@@ -17,7 +17,7 @@ object I18NUtil {
   }
 
   def getLocale(): Locale = Locale.getAvailableLocales().toList.filter { l =>
-    l.toLanguageTag == Props.get("language_tag", "en-GB")
+    l.toLanguageTag == Props.get("default_locale", "en-GB")
   }.headOption.getOrElse(Locale.ENGLISH)
 
   def currentLocale() : Locale = {
